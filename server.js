6da4d5c2d08a7e7809723f8662f37a30dcd04362
ankush bansal <ankushbansal1221@gmail.com>
2020-06-14 00:50:08 +0530

    code for heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+var router = express.Router();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/vuniFront'));
+// Start the app by listening on the default
+
+// Heroku port
+app.get('/', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/vuniFront/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
