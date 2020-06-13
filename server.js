a88f4f52ec040b3eda2411fa9033c24ac86b9c7a
ankush bansal <ankushbansal1221@gmail.com>
2020-06-14 01:02:24 +0530

    code change for heroku

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/vuniFront'));
+app.use(express.static(__dirname + '/dist/vuni'));
 // Start the app by listening on the default
 
 // Heroku port
 app.get('/', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/vuniFront/index.html'));
+res.sendFile(path.join(__dirname + '/dist/vuni/index.html'));
 });
 
 
