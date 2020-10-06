ee9b6e13bf42ed7030cf246a1340e28363b56e86
ankush bansal <ankushbansal1221@gmail.com>
2020-10-07 00:32:53 +0530

    latest

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   isAdmission: boolean;
+  activeElement: string;
   constructor() { }
 
   ngOnInit() {
@@ -18,5 +19,9 @@ export class SidebarComponent implements OnInit {
       this.isAdmission = false;
     }
 
+  }
+
+  actElement(name: string){
+this.activeElement = name;
   }
 }
